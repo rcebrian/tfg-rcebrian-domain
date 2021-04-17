d0b1b52b057c3fa90510873eeb585bf914735f65
rcebrian <rodrigocebriang@gmail.com>
2021-04-17 19:55:05 +0200

    :sparkles: devices model
    added devices model and updated ependencies

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,3 +4,4 @@ export { default as Group } from './group.model';
 export { default as UsersGroups } from './users-groups.model';
 export { default as Company } from './company.model';
 export { default as Login } from './login.model';
+export { default as Device } from './device.model';
